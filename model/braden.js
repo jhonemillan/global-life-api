0bd9ad5f071861fc84527d594fead7f40b98c096
jem <jhonemillan@gmail.com>
2018-01-30 02:39:55 -0500

    complete model

--- model/braden.js
+++ model/braden.js
@@ -13,7 +13,21 @@ module.exports = function(sequelize, DataTypes){
       friccion_cizallamiento:  { type: DataTypes.INTEGER, allowNull: true},
       puntajeTotal_Braden: { type: DataTypes.INTEGER, allowNull: true},
       observaciones_Braden: { type: DataTypes.STRING, allowNull: true},
-      fecha_creacion: {type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.Now}
+      banarse: { type: DataTypes.INTEGER, allowNull: true},
+      comer: { type: DataTypes.INTEGER, allowNull: true},
+      usar_retrete: { type: DataTypes.INTEGER, allowNull: true},
+      subir_escalera: { type: DataTypes.INTEGER, allowNull: true},
+      vestirse: { type: DataTypes.INTEGER, allowNull: true},
+      ctrl_deposicion: { type: DataTypes.INTEGER, allowNull: true},
+      ctrl_miccion: { type: DataTypes.INTEGER, allowNull: true},
+      caminar: { type: DataTypes.INTEGER, allowNull: true},
+      traslado_silla_cama: { type: DataTypes.INTEGER, allowNull: true},
+      puntajeTotal_val: { type: DataTypes.INTEGER, allowNull: true},
+      Observaciones_val : { type: DataTypes.STRING, allowNull: true},
+      PlandeCuidados : { type: DataTypes.STRING, allowNull: true},
+      EducacionPaciente : { type: DataTypes.STRING, allowNull: true},
+      fecha_creacion: {type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.Now},
+      iden_pac: { type: DataTypes.STRING, allowNull: true}
     });
   
     return valoracion;
--- routes/braden.js
+++ routes/braden.js
@@ -14,7 +14,7 @@ router.get('/getAll',(req, res)=>{
 
 router.post('/add',(req, res)=>{
     var val = models.braden.build({      
-      Fecha: req.body.fecha,
+      Fecha: req.body.Fecha,
       pielIntegra: req.body.pielIntegra,
       observacion_Piel: req.body.observacion_Piel,
       percepcion_sensorial: req.body.percepcion_sensorial,
@@ -24,7 +24,20 @@ router.post('/add',(req, res)=>{
       nutricion: req.body.nutricion,
       friccion_cizallamiento:  req.body.friccion_cizallamiento,
       puntajeTotal_Braden: req.body.puntajeTotal_Braden,
-      observaciones_Braden: req.body.observaciones_Braden
+      observaciones_Braden: req.body.observaciones_Braden,
+      banarse : req.body.banarse,
+      comer: req.body.comer,
+      usar_retrete: req.body.usar_retrete,
+      subir_escalera: req.body.subir_escalera,
+      vestirse: req.body.vestirse,
+      ctrl_deposicion: req.body.ctrl_deposicion,
+      ctrl_miccion: req.body.ctrl_miccion,
+      caminar: req.body.caminar,
+      traslado_silla_cama: req.body.traslado_silla_cama,
+      puntajeTotal_val: req.body.puntajeTotal_val,
+      Observaciones_val: req.body.Observaciones_val,
+      PlandeCuidados : req.body.PlandeCuidados,
+      iden_pac: req.body.iden_pac
     });
 
     val.save().then((datos)=> {
